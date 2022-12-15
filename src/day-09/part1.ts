4c9fecdf0890a04026ce9db37f3cb2d4224d07c6
Parkreiner <throwawayclover@gmail.com>
2022-12-15 12:05:48 -0500

    chore: Update class/type syntax to make code safer

--- src/day-09/part1.ts
+++ src/day-09/part1.ts
@@ -18,22 +18,22 @@ function assertIsDirection(value: unknown): asserts value is Direction {
 }
 
 function assertIsStep(value: unknown): asserts value is number {
-  if (!Number.isInteger(value) || (value as number) <= 0) {
+  if (typeof value !== "number" || !Number.isInteger(value) || value <= 0) {
     throw new Error(`Parsed value ${value} is not a valid step.`);
   }
 }
 
 class RopeSimulation {
-  grid: GridSpace[][];
-  head: GridCoordinate;
-  tail: GridCoordinate;
+  #grid: GridSpace[][];
+  #head: GridCoordinate;
+  #tail: GridCoordinate;
 
   constructor(gridSize: number) {
     if (!Number.isInteger(gridSize) || gridSize < 1) {
       throw new Error(`Provided size ${gridSize} is not a valid integer`);
     }
 
-    this.grid = new Array<GridSpace[]>(gridSize);
+    this.#grid = new Array<GridSpace[]>(gridSize);
     for (let i = 0; i < gridSize; i++) {
       const newRow = new Array<GridSpace>(gridSize);
 
@@ -42,14 +42,26 @@ class RopeSimulation {
         newRow[j] = { visited: atBottomLeftCorner };
       }
 
-      this.grid[i] = newRow;
+      this.#grid[i] = newRow;
     }
 
-    this.head = [0, gridSize - 1];
-    this.tail = [0, gridSize - 1];
+    this.#head = [0, gridSize - 1];
+    this.#tail = [0, gridSize - 1];
   }
 
-  processLines(lines: string[]): void {
+  get grid() {
+    return this.#grid.map((row) => [...row]);
+  }
+
+  get head() {
+    return [...this.#head] as const;
+  }
+
+  get tail() {
+    return [...this.#tail] as const;
+  }
+
+  processLines(lines: readonly string[]): void {
     for (const line of lines) {
       const [direction, rawSteps] = line.split(" ");
       const steps = parseInt(rawSteps || "", 10);
@@ -58,12 +70,12 @@ class RopeSimulation {
       assertIsStep(steps);
 
       for (let i = 0; i < steps; i++) {
-        this.updatePositions(direction);
+        this.#updatePositions(direction);
       }
     }
   }
 
-  areRopePartsTouching(): boolean {
+  #areRopePartsTouching(): boolean {
     const touchingX = false;
     const touchingY = false;
     const touchingDiagonal = false;
@@ -71,27 +83,27 @@ class RopeSimulation {
     return touchingX || touchingY || touchingDiagonal;
   }
 
-  updatePositions(dir: Direction): void {
+  #updatePositions(dir: Direction): void {
     switch (dir) {
       case "u": {
-        this.head[1]++;
-        if (this.areRopePartsTouching()) break;
+        this.#head[1]++;
+        if (this.#areRopePartsTouching()) break;
 
         break;
       }
 
       case "d": {
-        this.head[1]--;
+        this.#head[1]--;
         break;
       }
 
       case "l": {
-        this.head[0]--;
+        this.#head[0]--;
         break;
       }
 
       case "r": {
-        this.head[0]++;
+        this.#head[0]++;
         break;
       }
 
@@ -106,7 +118,7 @@ class RopeSimulation {
       return prevSum + (gs.visited ? 1 : 0);
     };
 
-    return this.grid.reduce((sum, row) => row.reduce(addIfVisited, sum), 0);
+    return this.#grid.reduce((sum, row) => row.reduce(addIfVisited, sum), 0);
   }
 }
 
